4ad5d1ee063898658e1dc9e6885961b36f0b5bc9
Seoryeon <79080533+sryeon1021@users.noreply.github.com>
2021-02-15 17:47:07 +0900

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,548 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hello world')
+
+
+# In[2]:
+
+
+a='life is too short'
+
+
+# In[3]:
+
+
+a[3]
+
+
+# In[4]:
+
+
+a=[1,2,3]
+
+
+# In[5]:
+
+
+a.insert(3.5)
+
+
+# In[6]:
+
+
+a.insert(0,4)
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+a=[1,2,3,1,2,3]
+
+
+# In[9]:
+
+
+a.remove(3)
+
+
+# In[10]:
+
+
+a
+
+
+# In[11]:
+
+
+a.remove(3)
+
+
+# In[12]:
+
+
+a
+
+
+# In[13]:
+
+
+a.pop()
+
+
+# In[14]:
+
+
+a
+
+
+# In[15]:
+
+
+a.insert(5)
+
+
+# In[16]:
+
+
+a.insert(2,5)
+
+
+# In[17]:
+
+
+a
+
+
+# In[18]:
+
+
+a=[1,2,3,4,5]
+
+
+# In[19]:
+
+
+a.insert(2,8)
+
+
+# In[20]:
+
+
+a
+
+
+# In[21]:
+
+
+a.pop()
+
+
+# In[22]:
+
+
+a
+
+
+# In[23]:
+
+
+a.count(1)
+
+
+# In[24]:
+
+
+a.count(1,2)
+
+
+# In[25]:
+
+
+a.count(5)
+
+
+# In[26]:
+
+
+a.extend(6,7)
+
+
+# In[27]:
+
+
+a.extend([6,7])
+
+
+# In[28]:
+
+
+a
+
+
+# In[29]:
+
+
+t1=()
+t2=(1,)
+t3=(1,2,3)
+t4=1,2,3
+t5=('a','b',('ab','cd'))
+
+
+# In[30]:
+
+
+t1=(1,2,'a','b')
+
+
+# In[33]:
+
+
+del t1[0]
+
+
+# In[34]:
+
+
+len(t5)
+
+
+# In[36]:
+
+
+t(2)*3
+
+
+# t2*3
+
+# In[37]:
+
+
+t2*3
+
+
+# In[38]:
+
+
+dic={'name':'pey', 'phone':'1111', 'birth':"oct"}
+
+
+# In[39]:
+
+
+a={1:'hi'}
+
+
+# In[40]:
+
+
+a[2]='bye'
+
+
+# In[41]:
+
+
+a
+
+
+# In[45]:
+
+
+a['name'
+
+
+# In[ ]:
+
+
+
+
+
+# In[46]:
+
+
+del a[1]
+
+
+# In[47]:
+
+
+a
+
+
+# In[49]:
+
+
+animal={'maya':'dog','mochi':'cat','guppy':'fish'}
+
+
+# In[50]:
+
+
+animal
+
+
+# In[ ]:
+
+
+
+
+
+# In[51]:
+
+
+animal['maya']
+
+
+# In[52]:
+
+
+animal['cat']
+
+
+# In[53]:
+
+
+animal['mochi']
+
+
+# In[54]:
+
+
+del animal['mochi']
+
+
+# In[55]:
+
+
+animal
+
+
+# In[56]:
+
+
+animal['marola']='dog'
+
+
+# In[57]:
+
+
+animal
+
+
+# In[58]:
+
+
+s1=set([1,2,3])
+
+
+# In[59]:
+
+
+s1
+
+
+# In[62]:
+
+
+s2=set("hello")
+
+
+# In[63]:
+
+
+s2
+
+
+# In[65]:
+
+
+bool('python')
+
+
+# In[66]:
+
+
+bool('python')
+
+
+# In[67]:
+
+
+bool("")
+
+
+# In[ ]:
+
+
+
+
+
+# In[68]:
+
+
+(80+75+55)/3
+
+
+# In[69]:
+
+
+jumin="881120-1068234"
+
+
+# In[70]:
+
+
+junum=jumin[:6]
+num=jumin[7:]
+
+
+# In[73]:
+
+
+money= 2000
+card= True
+
+if money >= 3000 and card:
+    print ('taxi')
+else:
+    print ('walk')
+
+
+# In[78]:
+
+
+pocket= ['paper','cellphone','money']
+
+if 'money' in pocket:
+    pass
+else:
+    print('walk')
+
+
+# In[ ]:
+
+
+
+
+
+# In[80]:
+
+
+pocket=['paper','cell','money']
+card= True 
+if 'money' in pocket:
+    print ('taxi')
+else: 
+        print ('walk')
+
+
+# In[81]:
+
+
+pocket=['paper','cellphone']
+card= True
+if 'money' in pocket:
+    print('taxi')
+elif card:
+    print('taxi')
+else:
+    print('walk')
+
+
+# In[82]:
+
+
+if score >=60:
+    messagee =
+
+
+# In[ ]:
+
+
+
+
+
+# In[83]:
+
+
+treehit = 0
+while treehit<10:
+    treehit =treehit +1
+    prehit
+    if treehit ==10
+    it
+    
+
+    
+
+
+# number=0
+# while number !=4:
+#     print (prompt)
+#     number = int(input())
+#     
+
+# In[91]:
+
+
+number = 3
+while number != 4 :
+    print (4)
+    number = int(input())
+
+
+# In[ ]:
+
+
+
+
+
+# In[97]:
+
+
+coffee= 10 
+money = 300
+
+while money:
+    print ('got money give coffee')
+    coffee = coffee - 1
+    print ('leftover coffee is %d amount.' %coffee')
+    if coffee== 0:
+    print ('coffee is done')
+    break
+    
+
+
+# In[99]:
+
+
+coffee.py 
+coffee=10 
+while True:
+    money=int(input(
+        
+
+
+# In[105]:
+
+
+a= 3
+while a< 10:
+    a=a+1
+    if a % 2==0: continue 
+
+
+# In[ ]:
+
+
+while True :
+    print ('Ctrl + C 를 눌러야 while 문을 빠져나갈수있음 ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
